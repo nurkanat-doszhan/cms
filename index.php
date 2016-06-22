5e16c2cd3edb45187c1a067f5c57c148b778d969
ДосжанН <nurkanat.doszhan@mail.ru>
2016-06-22 20:20:50 +0600

    Cms

--- index.php
+++ index.php
@@ -1,2 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="cmsStyle.css">
+</head>
+<body>
+    <div class="content">
+        <h3>Lorem debitis facilis aperiam odio, temporibus.</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br>
+            Accusamus cum debitis eligendi enim illum magnam, nam quasi quos vitae voluptas.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br>
+            Aliquid debitis delectus facilis, nesciunt nobis pariatur ratione reprehenderit<br>
+            rerum voluptatibus! Accusantium animi aperiam distinctio earum fuga hic iure<br>
+            laudantium molestias odio praesentium repellendus suscipit velit vero, voluptate, voluptatem.<br>
+            Assumenda deleniti, temporibus.</p>  
+    </div>
+
+</body>
+</html>
+
 <?php
 
+
+?>
