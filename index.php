7b11e6cef21b7b606d05faad7a642971e67f4399
ДосжанН <nurkanat.doszhan@mail.ru>
2016-06-23 19:30:39 +0600

    Cms

--- index.php
+++ index.php
@@ -8,15 +8,14 @@
 <body>
     <div class="content">
         <h3>Lorem debitis facilis aperiam odio, temporibus.</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br>
-            Accusamus cum debitis eligendi enim illum magnam, nam quasi quos vitae voluptas.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br>
-            Aliquid debitis delectus facilis, nesciunt nobis pariatur ratione reprehenderit<br>
-            rerum voluptatibus! Accusantium animi aperiam distinctio earum fuga hic iure<br>
+        <p class="line">Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br>
+            Accusamus cum debitis eligendi enim illum magnam, nam quasi quos vitae voluptas.<br><br>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br>
+            Aliquid debitis delectus facilis, nesciunt nobis pariatur ratione reprehenderit rerum voluptatibus!<br>
+            Accusantium animi aperiam distinctio earum fuga hic iure<br>
             laudantium molestias odio praesentium repellendus suscipit velit vero, voluptate, voluptatem.<br>
             Assumenda deleniti, temporibus.</p>  
     </div>
-
 </body>
 </html>
 
